51be79d8045829789c02d9cba0db7be70fb36981
datlmc <datlmc63@gmail.com>
2018-10-19 00:14:22 +0700

    add

---
+++ array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[10];
+            number[0] = 0;
+            number[1] = 1;
+            number[2] = 2;
+            number[3] = 3;
+            number[4] = 4;
+            ////khuc sau rong la null =0
+            string[] names = new string[] { "\ntu,nhi,lam,hung,phuong" };
+            foreach (string name in names)
+            {
+
+
+                Console.WriteLine("name :" + name);
+            }
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.WriteLine("{0} is {1}", i, number[i]);
+
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+}
